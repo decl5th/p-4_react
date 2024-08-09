39b06e7ddd8f204e9639250c2800a7ac9b5f8817
decl5th <alsgur6624@gmail.com>
2024-08-09 16:04:41 +0900

    지도 마커 출력 및 마커 이미지 설정

--- react_project-master/src/main/pages/Main.js
+++ react_project-master/src/main/pages/Main.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import MapContainer from "../../map/KakaoMap";
+import KakaoMap from "../../map/KakaoMap";
+
+
+const markers = [
+{lat:37.55772142387766, lng:126.94321920274982},
+{lat:37.55648123246987, lng:126.94519221656536,
+  image:
+  "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png"
+},
+{lat:37.55783517406359, lng:126.94560997964005},
+];
+
+const options = {
+  center: { lat:37.55772142387766, lng:126.94321920274982  } ,
+  zoom: 4, 
+  marker: markers,
+  markerImage: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png",
+};
 
 const Main = () => {
-  return <MapContainer />;
+  return <KakaoMap {...options}/>;
 };
 
 export default React.memo(Main);
--- react_project-master/src/map/KakaoMap.js
+++ react_project-master/src/map/KakaoMap.js
@@ -1,28 +1,64 @@
 /* eslint-disable no-undef */
 import React, { useEffect, useCallback, useRef } from 'react';
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 
 const MapArea = styled.div`
-
+  width: ${({width}) => width ?? '100%' }; 
+  height: ${({height}) => height ?? '38.5rem'};
 `;
 
-const KakaoMap = () => {
+const KakaoMap = ({width, height, center, zoom, marker, markerImage}) => {
   const mapRef = useRef(null);
 
   useEffect(() => {
     const mapEl = mapRef.current;
-    const lat = 37.557756188912954;
-    const lng = 126.94062742683245;
-    mapEl.style.width = 1000 + 'px';
-    mapEl.style.height = 500 + 'px';
-    const position = new kakao.maps.LatLng(lat, lng);
+    
+    // 지도 가운데 배치 B
+    const position = new kakao.maps.LatLng(
+      center?.lat ?? 37.557756188912954,
+      center?.lng ?? 126.94062742683245,
+    );
     const map = new kakao.maps.Map(mapEl, {
       center: position,
-      level: 3,
+      level: zoom || 3,
     });
-  }, [mapRef]);
+    // 지도 가운데 배치 D
 
-  return <div ref={mapRef}></div>;
+    // 마커 출력 B
+    if (marker) {
+      let _markers = marker;
+      if (!Array.isArray(marker)) _markers = [marker];
+
+      const markers = _markers.map(m => {
+        const {lat, lng, image} = m;
+        const options = {
+            position: new kakao.maps.LatLng(lat, lng),
+          };
+        const _marker = new kakao.maps.Marker(options);
+        
+        // 마커 이미지 처리 B
+          const mi = image ? image : markerImage;
+          if (mi) {
+            const mIcon = new kakao.maps.MarkerImage(mi, new kakao.maps.Size(64, 69), 
+              {offset: new kakao.maps.Point(27,69)},
+            );
+
+            options.image = mIcon
+          }
+        // 마커 이미지 처리 D
+        
+        _marker.setMap(map);
+
+        return _marker;
+        
+      });
+
+    }
+
+    // 마커 출력 D
+  }, [mapRef, center, zoom, marker, markerImage]);
+
+  return <MapArea ref={mapRef} width={width} height={height} />;
 };
 
 export default React.memo(KakaoMap);
